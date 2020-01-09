10a072056412a835b758ab00d695fcae652b7d9e
soutahatano <yuzuriha3836@gmail.com>
2020-01-09 13:05:58 +0900

    db seeds

---
+++ db/migrate/20200109035306_create_cities.rb
@@ -0,0 +1,9 @@
+class CreateCities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cities do |t|
+      t.integer :pref_id
+      t.string :name, limit: 16
+      t.timestamps
+    end
+  end
+end
