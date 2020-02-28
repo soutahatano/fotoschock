8ea1b2636ba046dd3859160f0ef194f1a7d56482
soutahatano <yuzuriha3836@gmail.com>
2020-02-28 10:34:23 +0900

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "csv"
 CSVROW_PREFNAME = 6
 CSVROW_CITYNAME = 7
 
-CSV.foreach('db/seeds/ken_all.csv', encoding: "Shift_JIS:UTF-8") do |row|
+CSV.foreach(Rails.root + 'db/seeds/ken_all.csv', encoding: "Shift_JIS:UTF-8") do |row|
   prefName = row[CSVROW_PREFNAME]
   cityName = row[CSVROW_CITYNAME]
   pref = Pref.find_or_create_by(name: prefName)
