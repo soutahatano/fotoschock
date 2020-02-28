3cfe6d2092b36fae302d48bb9e587dee51827e18
soutahatano <yuzuriha3836@gmail.com>
2020-02-28 12:54:56 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,2 @@
-require "csv"
-
-CSVROW_PREFNAME = 6
-CSVROW_CITYNAME = 7
-
-CSV.foreach(Rails.root + 'db/seeds/ken_all.csv', encoding: "Shift_JIS:UTF-8") do |row|
-  prefName = row[CSVROW_PREFNAME]
-  cityName = row[CSVROW_CITYNAME]
-  pref = Pref.find_or_create_by(name: prefName)
-  City.find_or_create_by(name: cityName, pref_id: pref.id)
-end
+require './db/seeds/pref.rb'
+require './db/seeds/city.rb'
