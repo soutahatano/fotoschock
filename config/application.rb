06d39e2b75c07dcf806a0e94cb08269d4d389c4e
soutahatano <yuzuriha3836@gmail.com>
2020-03-05 20:36:27 +0900

    へろく　設定

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Fotoschock
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    config.assets.initialize_on_precompile = false
   end
 end
