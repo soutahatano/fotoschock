95d1f0526668cba5421abe33b79c9eae5a807b0f
soutahatano <yuzuriha3836@gmail.com>
2020-03-06 11:33:53 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Fotoschock
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-    config.assets.initialize_on_precompile = false
   end
 end
