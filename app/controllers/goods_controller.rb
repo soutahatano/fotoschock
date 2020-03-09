9f418bac868763e602be36e9c0e82bc926516cf2
soutahatano <yuzuriha3836@gmail.com>
2020-03-09 14:03:33 +0900

    good

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -5,16 +5,16 @@ class GoodsController < ApplicationController
   def create
     @good = Good.new(user_id: current_user.id, post_id: params[:post_id])
     if @good.save
-      current_user.good
-      current_user.save
+      @good.post.user.good
+      @good.post.user.save
     end
   end
 
   def destroy
     @good = Good.find_by(user_id: params[:user_id], post_id: params[:post_id])
     if @good.destroy
-      current_user.delgood
-      current_user.save
+      @good.post.user.delgood
+      @good.post.user.save
     end
   end
 
